dcf4cc3dfef3e6db757fe49ca08207e093bf3ddf
kate <katarzynabeyger@wp.pl>
2025-11-13 12:46:20 +0100

    Dodano obsługe pliku Odkurzacz.java

--- src/Odkurzacz.java
+++ src/Odkurzacz.java
@@ -1,3 +1,19 @@
 public class Odkurzacz extends Bazowa{
+    boolean stan = false;
 
+    public boolean on(){
+        if(stan == false){
+            stan = true;
+            wyswietlanie("Włączono odkurzacz");
+        }
+        return stan;
+    }
+
+    public boolean off(){
+        if(stan == true){
+            stan = false;
+            wyswietlanie("Wyłączono odkurzacz");
+        }
+        return stan;
+    }
 }
